4ad4994b3bdc91a7b030b2d80137340fca310c8e
Fedele <fedmau97tab@gmail.com>
2020-10-29 11:35:36 +0100

    DatabaseFiles

---
+++ Database/presetstore_collezionista.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: presetstore
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `collezionista`
+--
+
+LOCK TABLES `collezionista` WRITE;
+/*!40000 ALTER TABLE `collezionista` DISABLE KEYS */;
+INSERT INTO `collezionista` VALUES ('alfa1','5116e40694ac48f654cb7b6816177e0e717237c6','ITALIA','Antonio','Alfano','alfano75@unisa.it'),('ali','b78b647728101ba462182b4c7e5b2ca57b9f5a99','Bari','aliBabba','baba','allaahkbar@gerusalemme.com'),('emanuele1','1dd09baa19dc7688f96e2ea45033603921cd7b83','FRANCIA','Emanuele','Fittpaldi','emanuele@.it'),('EmanueleFitti','52d3bae5c1352c5f807c79f488efa95d1c522870','Italiana','Emanuele','Fittipaldi','emanuele@email.it'),('frickfrack','d67cca5aaed6ead2e1c1c6b6e1d20a3d14c9622f','BRASILE','Eugenio','Sottile','sottil@.it'),('gesu','1a970306086aed5c259028083d5815ae6fa8b93f','Algeria','Antonio','Alfano','gesu@alice.it'),('giuseppe','a94a8fe5ccb19ba61c4c0873d391e987982fbbd3','Italia','test','test','test@alice.com'),('master97','b76976bee099642e1d462e92dd34d7505967ed42','NORVEGIA','Roberto','Fabiano','rob98@.it'),('medaka12','c7d8a6d722a1ec9a16fae165177c418d4fd63175','GIAPPONE','Annalisa','Alfano','anny96@.it'),('peppe1','32c8bbff09c356265a96fb8385cfa141c9d92f76','SPAGNA','Giuseppe','Martinelli','martinelli@.it'),('pk22','c02b74168b9adde02a4c437ad8236e42fb7f7c3f','SVEZIA','Michael','Fox','fox99@.it'),('shadow','7b2de7105c113bba5e7638f6518254783f97892e','USA','Luigi','Sorrentino','wubs@.it'),('test','a94a8fe5ccb19ba61c4c0873d391e987982fbbd3','test','test','test','test@test.it'),('test3','3ebfa301dc59196f18593c45e519287a23297589','Italia','Gennaro','tsw','test3@tsw.it');
+/*!40000 ALTER TABLE `collezionista` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-29 11:34:48
---
+++ Database/presetstore_prodotto.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: presetstore
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `prodotto`
+--
+
+LOCK TABLES `prodotto` WRITE;
+/*!40000 ALTER TABLE `prodotto` DISABLE KEYS */;
+INSERT INTO `prodotto` VALUES (1,'Black Paris','Ispirato all\'account instagram @blackparis',2100),(2,'White Paris','Ispirato all\'account instagram @whiteparis.',1000),(3,'Cinematic Night','Per dare un look da film ai tuoi scatti notturni',1500),(4,'Moody Portrait','Per dare un look malinconico ai tuoi ritratti',2500),(5,'Urban Streets','Trasforma i tuoi scatti in città con un look puramente urbano',2500),(6,'Joker Cinematic','Preset ispirato al mood del premio Oscar Joker',5000),(7,'Dark Sea','Dai un look drammatico alle tue foto al mare',1500),(8,'Urbex Activity','Preset adatto alle esplorazioni urbane',1000),(9,'Food Blogger','Preset per i tuoi scatti al cibo',1700),(10,'Bali Inspired','Dai alle tue foto quell\'atmosfera che rende unica Bali',1500),(11,'Brandon Woelfel','Ispirato all\'account instagram @brandonwoelfel',2000),(12,'Desert Sands','Preset per dare toni tendenti all\' Orange',500);
+/*!40000 ALTER TABLE `prodotto` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-29 11:34:49
